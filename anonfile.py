cc6550a5cf1219a5b2354d281f9572512d46bf27
Nick <nstrydom@gmail.com>
2019-02-22 15:18:18 -0600

    Added a custom timeout option

--- anonfile.py
+++ anonfile.py
@@ -5,11 +5,28 @@ import json
 from bs4 import BeautifulSoup
 
 class AnonFile():
-    def __init__(self):
+    def __init__(self, api_key):
+        # Api endpoint
         self.anonfile_endpoint_url = 'https://anonfile.com/api/'
-        self.api_key = ''
 
-    # takes file path and uploads file returning the url
+        # User specific api key
+        self.api_key = api_key
+
+        # Set timeout (connect, read)
+        self.timeout = (5, 5)
+
+    # Custom timemout constructor
+    def __init__(self, api_key , custom_timeout):
+        # Api endpoint
+        self.anonfile_endpoint_url = 'https://anonfile.com/api/'
+
+        # User specific api key
+        self.api_key = api_key
+
+        # Set timeout (connect, read)
+        self.timeout = custom_timeout
+
+    # Takes file path and uploads file returning the url
     # to download file after the upload is complete, elses
     # return None if exception is thrown
     def upload_file(self, file_path):
@@ -27,11 +44,11 @@ class AnonFile():
 
             return None
 
-    # automatically downloads from anonfile.com based
+    # Automatically downloads from anonfile.com based
     # on the given url
     def download_file(self, url):
         try:
-            reponse = requests.get(url, timeout=(5, 5))
+            reponse = requests.get(url, timeout=self.timeout)
             soup = BeautifulSoup(reponse.text, 'lxml')
             download_url = soup.find_all('a')[0].attrs['href']
 
