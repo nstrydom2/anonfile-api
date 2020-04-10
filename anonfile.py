8adb58a9834c0955a543020cb1a85d4ed70c9fc0
Nick <nstrydom@gmail.com>
2020-04-10 04:11:18 -0500

    Add license and comments to anonfile.py

--- anonfile.py
+++ anonfile.py
@@ -1,3 +1,25 @@
+ # The MIT License
+ #
+ # Copyright (c) 2020, Nicholas Bruce Strydom
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documentation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to whom the Software is
+ # furnished to do so, subject to the following conditions:
+ #
+ # The above copyright notice and this permission notice shall be included in
+ # all copies or substantial portions of the Software.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ # THE SOFTWARE.
+
 import wget
 import requests
 
@@ -35,6 +57,8 @@ class AnonFile():
             # Set timeout (connect, read)
             self.timeout = (5, 5)
 
+    # Custom annotation to ensure api_key has been
+    # initialized before using the Api
     def authenticated(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
