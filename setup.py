ce8c71baf18065cd2b03b4bff32a7e9019de3027
Nick <nstrydom@gmail.com>
2020-04-12 01:01:18 -0500

    Configure setup.py file

--- setup.py
+++ setup.py
@@ -1,39 +1,22 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-
     long_description = fh.read()
 
 setuptools.setup(
-
-     name='dokr',
-
-     version='0.1',
-
-     scripts=['dokr'] ,
-
-     author="Deepak Kumar",
-
-     author_email="deepak.kumar.iet@gmail.com",
-
-     description="A Docker and AWS utility package",
-
-     long_description=long_description,
-
-   long_description_content_type="text/markdown",
-
-     url="https://github.com/javatechy/dokr",
-
-     packages=setuptools.find_packages(),
-
-     classifiers=[
-
-         "Programming Language :: Python :: 3",
-
-         "License :: OSI Approved :: MIT License",
-
-         "Operating System :: OS Independent",
-
-     ],
-
- )
+    name='anonfile-api',
+    version='0.1',
+    scripts=['anonfile_api'],
+    author="Nicholas Strydom",
+    author_email="nstrydom@gmail.com",
+    description="An unofficial library that wraps the Anonfile.com REST Api.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nstrydom2/anonfile-api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
