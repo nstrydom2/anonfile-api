163bc01cf8c1758efa9267cf2e2e3a001a950e18
Nick <nstrydom@gmail.com>
2020-04-12 02:00:16 -0500

    Reconfigure name for pypi package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='anonfile-api',
+    name='anonfile',
     version='0.1',
     scripts=['anonfile_api'],
     author="Nicholas Strydom",
