766a5a6c2ba483c4090be7f99166f578b4c78109
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-05-02 23:01:49 +0200

    Add new option to command line interface for passing the API token to pytest

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--token',
+        action='append',
+        default=None,
+        help="Secret anonfile.com API token."
+    )
+
+def pytest_generate_tests(metafunc):
+    if 'token' in metafunc.fixturenames:
+        metafunc.parametrize('token', metafunc.config.getoption('token'))
