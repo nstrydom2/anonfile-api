ed135eaa1bf581058121817acabe176d8f5e769f
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-05-03 22:42:08 +0200

    Update test_download and test_path

--- tests/test_anonfile.py
+++ tests/test_anonfile.py
@@ -14,7 +14,7 @@ class TestAnonFile(unittest.TestCase):
     def setUp(self):
         self.anon = AnonFile(TOKEN)
         self.test_file = Path("tests/test.txt")
-        self.test_path = "https://anonfiles.com/9ee1jcu6u9/test_txt"
+        self.test_path = "https://anonfiles.com/93k5x1ucu0/test_txt"
 
     def test_upload(self):
         result = self.anon.upload(self.test_file)
@@ -23,6 +23,6 @@ class TestAnonFile(unittest.TestCase):
 
     def test_download(self):
         result = self.anon.download(self.test_path)
-        self.assertTrue(result.exists(), msg="Download not successful.")
-        self.assertEqual(result.name, self.test_file.name, msg="Different file in download path detected.")
-        result.unlink(missing_ok=True)        
+        self.assertTrue(result.file_path.exists(), msg="Download not successful.")
+        self.assertEqual(result.file_path.name, self.test_file.name, msg="Different file in download path detected.")
+        result.file_path.unlink(missing_ok=True)   
