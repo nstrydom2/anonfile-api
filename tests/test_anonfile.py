b8f594d6acd970bbcb00b3a2367b34a309974c41
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-05-11 13:16:34 +0200

    Enable progressbars in test suite

--- tests/test_anonfile.py
+++ tests/test_anonfile.py
@@ -18,12 +18,12 @@ class TestAnonFile(unittest.TestCase):
         self.test_path = "https://anonfiles.com/93k5x1ucu0/test_txt"
 
     def test_upload(self):
-        result = self.anon.upload(self.test_file)
+        result = self.anon.upload(self.test_file, progressbar=True)
         self.assertTrue(result.status, msg="Expected 200 HTTP Error Code")
         self.assertTrue(all([result.url.scheme, result.url.netloc, result.url.path]), msg="Invalid URL.")
 
     def test_download(self):
-        result = self.anon.download(self.test_path)
+        result = self.anon.download(self.test_path, progressbar=True)
         self.assertTrue(result.file_path.exists(), msg="Download not successful.")
         self.assertEqual(result.file_path.name, self.test_file.name, msg="Different file in download path detected.")
         result.file_path.unlink()
