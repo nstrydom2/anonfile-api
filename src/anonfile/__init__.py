09857f81ce08439bbf379433ee10d7585dd8a0c6
nstrydom2 <nstrydom@gmail.com>
2021-05-13 03:34:36 -0400

    Add user friendly error messages for CLI when missing args

--- src/anonfile/__init__.py
+++ src/anonfile/__init__.py
@@ -6,34 +6,49 @@ from pathlib import Path
 from .anonfile import *
 from .anonfile import __version__
 
+
 def main():
     parser = argparse.ArgumentParser(prog=package_name)
+    parser._positionals.title = 'Commands'
+    parser._optionals.title = 'Arguments'
+
+    parser.add_argument('-v', '--version', action='version', version=f"%(prog)s {__version__}")
+    parser.add_argument('-V', '--verbose', action='store_true', help="increase output verbosity")
+    parser.add_argument('-t', '--token', type=str, default='secret', help="configure an API token (optional)")
+
     subparser = parser.add_subparsers(dest='command')
-
-    parser.add_argument('--version', action='version', version=f"%(prog)s {__version__}")
-    parser.add_argument('--verbose', action='store_true', help="increase output verbosity")
-    parser.add_argument('--token', type=str, default='secret', help="configure an API token (optional)")
-
     upload_parser = subparser.add_parser('upload', help="upload a file to https://anonfiles.com")
-    upload_parser.add_argument('--file', nargs='+', type=Path, help="one or more file to upload.")
+    upload_parser.add_argument('-f', '--file', nargs='+', type=Path, help="one or more files to upload.", required=True)
 
     download_parser = subparser.add_parser('download', help="download a file from https://anonfiles.com")
-    download_parser.add_argument('--url', nargs='+', type=str, help="one or more URL to download")
-    download_parser.add_argument('--path', type=Path, default=Path.cwd(), help="download directory (CWD by default)")
+    download_parser.add_argument('-u', '--url', nargs='+', type=str, help="one or more URLs to download", required=True)
+    download_parser.add_argument('-p', '--path', type=Path, default=Path.cwd(), help="download directory (CWD by default)")
 
-    args = parser.parse_args()
+    try:
+        args = parser.parse_args()
+        anon = AnonFile(args.token)
 
-    anon = AnonFile(args.token)
+        if args.command is None:
+            raise UserWarning("missing a command")
 
-    if args.command == 'upload':
-        for file in args.file:
-            upload = anon.upload(file, progressbar=args.verbose)
-            print(f"URL: {upload.url.geturl()}")
+        if args.command == 'upload':
+            for file in args.file:
+                upload = anon.upload(file, progressbar=args.verbose)
+                print(f"URL: {upload.url.geturl()}")
 
-    if args.command == 'download':
-        for url in args.url:
-            download = anon.download(url, args.path, progressbar=args.verbose)
-            print(f"File: {download.file_path}")
+        if args.command == 'download':
+            for url in args.url:
+                download = anon.download(url, args.path, progressbar=args.verbose)
+                print(f"File: {download.file_path}")
+
+    except UserWarning as bad_human:
+        print(f"error: {bad_human}")
+        parser.print_help(sys.stderr)
+        sys.exit(2)
+
+    except Exception as ex:
+        print(f"error: {ex}")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
