800821bc37929a96bb79a044077d0323cc284b12
Nicholas Strydom <nstrydom@gmail.com>
2023-05-24 21:26:53 -0500

    Mock `Session.get()` method

--- tests/test_anonfile.py
+++ tests/test_anonfile.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import hashlib
+import json
 import random
 import subprocess
 import unittest
 from pathlib import Path
 from unittest import skip
+from unittest.mock import patch, Mock, MagicMock
 
 from faker import Faker
+from requests import Response
 
 from src.anonfile import AnonFile, get_logfile_path
 
@@ -42,9 +45,9 @@ class TestAnonFileLibrary(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.anon = init_anon()
-        cls.test_file = Path("tests/test.txt")
+        cls.test_file = Path("tests/topsecret.mp4")
         cls.test_small_file = "https://anonfiles.com/93k5x1ucu0/test_txt"
-        cls.test_med_file = "https://anonfiles.com/Z6j7c9V6x5/topsecret_mp4"
+        cls.test_med_file = "https://anonfiles.com/P0mev3tfz7/topsecret_mp4"
         cls.garbage = []
 
     def test_upload(self):
@@ -52,16 +55,63 @@ class TestAnonFileLibrary(unittest.TestCase):
         self.assertTrue(upload.status, msg="Expected 200 HTTP Error Code")
         self.assertTrue(all([upload.url.scheme, upload.url.netloc, upload.url.path]), msg="Invalid URL.")
 
-    def test_preview(self):
-        preview = self.anon.preview(self.test_small_file)
-        self.assertTrue(preview.status, msg="Error in status property.")
-        self.assertEqual(self.test_small_file, preview.url.geturl(), msg="Error in URL property.")
-        self.assertEqual("93k5x1ucu0", preview.id, msg="Error in ID property.")
-        self.assertEqual("test.txt", preview.file_path.name, msg="Error in name property.")
-        self.assertEqual(271, preview.size, msg="Error in size property.")
+    @patch('anonfile.requests.Session.get')
+    def test_preview(self, mocked_session_get):
+        json_content = open('./preview.json', encoding='utf-8').read()
+        html_content = open('./preview.html', 'r', encoding='utf-8').read()
 
-    def test_download(self):
-        download = self.anon.download(self.test_small_file, progressbar=True, enable_logging=True)
+        json_response = Mock(spec=Response)
+        json_response.__enter__ = MagicMock(return_value=json_response)
+        json_response.__exit__ = MagicMock()
+        json_response.status_code = 200
+        json_response.json.return_value = json.loads(json_content)
+
+        html_response = Mock(spec=Response)
+        html_response.__enter__ = MagicMock(return_value=html_response)
+        html_response.__exit__ = MagicMock()
+        html_response.status_code = 200
+        html_response.text = html_content
+
+        mocked_session_get.side_effect = [json_response, html_response]
+
+        preview = self.anon.preview(self.test_med_file)
+        self.assertTrue(preview.status, msg="Error in status property.")
+        self.assertEqual(self.test_med_file, preview.url.geturl(), msg="Error in URL property.")
+        self.assertEqual("P0mev3tfz7", preview.id, msg="Error in ID property.")
+        self.assertEqual("original_topsecret.mp4", preview.file_path.name, msg="Error in name property.")
+        self.assertEqual(3537832, preview.size, msg="Error in size property.")
+
+    @patch('anonfile.requests.Session.get')
+    def test_download(self, mocked_session_get):
+        json_content = open('./preview.json', encoding='utf-8').read()
+        html_content = open('./preview.html', 'r', encoding='utf-8').read()
+
+        json_response = Mock(spec=Response)
+        json_response.__enter__ = MagicMock(return_value=json_response)
+        json_response.__exit__ = MagicMock()
+        json_response.status_code = 200
+        json_response.json.return_value = json.loads(json_content)
+
+        html_response = Mock(spec=Response)
+        html_response.__enter__ = MagicMock(return_value=html_response)
+        html_response.__exit__ = MagicMock()
+        html_response.status_code = 200
+        html_response.text = html_content
+
+        download_bytes = open('./original_topsecret.mp4', 'rb').read()
+
+        # Create a generator to mimic streamed content
+        response_data = (chunk for chunk in download_bytes)
+
+        # Mock the Response object's `iter_content` method to return the generator
+        download_response = Mock(spec=Response)
+        download_response.__enter__ = MagicMock(return_value=download_response)
+        download_response.__exit__ = MagicMock()
+        download_response.iter_content.return_value = response_data
+
+        mocked_session_get.side_effect = [json_response, html_response, download_response]
+
+        download = self.anon.download(self.test_med_file, progressbar=True, enable_logging=True)
         self.assertTrue(download.file_path.exists(), msg="Download not successful.")
         self.assertEqual(download.file_path.name, self.test_file.name, msg="Different file in download path detected.")
         self.garbage.append(download.file_path)
